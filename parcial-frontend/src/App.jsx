7778276695b5c55ffd928edc0a10809f41b6b235
agusbarca <agustinabelenbarca@gmail>
2023-08-16 14:34:19 -0300

    First Commit

---
+++ parcial-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card/Card'
+import Form from './Components/Form/Form'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Form />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ parcial-frontend/src/Components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>
+      Hola soy el form
+    </form>
+  )
+}
+
+export default Form
